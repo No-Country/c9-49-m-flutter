e032c8946a048c090ff45c3eed9e5af61e87200d
Raul Ereño <raulereno@gmail.com>
2023-02-27 19:44:17 -0300

    Arreglando bug de imagenes, archivos modificados: - userdata.utils.dart Agregando condicion de filtrado para que no traiga al admin en los chats

--- lib/utils/userdata.utils.dart
+++ lib/utils/userdata.utils.dart
@@ -2,5 +2,6 @@ abstract class UserDataUtils {
   static String getChannelImage() => getUserImage('');
 
   static getUserImage(String? value) =>
-      'https://ui-avatars.com/api/?name=$value&background=blue';
+      "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png";
 }
+      // 'https://ui-avatars.com/api/?name=$value&background=blue';
