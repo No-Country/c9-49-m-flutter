5c3a7b75b9c900256ae1e9053f2728b018262794
Tomás Salina <salinatomass53@gmail.com>
2023-03-01 11:26:55 -0300

    Connect screen already looks like the design in figma

--- lib/screens/Connect/connect_screen.dart
+++ lib/screens/Connect/connect_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
+// Page:
 import '../Chat/channel_page.dart';
+
+// Theme:
+import '../../theme/colors_theme.dart';
+
+// Types:
 import '../../types/user.dart';
 
 class ConnectScreen extends StatelessWidget {
@@ -32,7 +38,7 @@ class UserListPage extends StatefulWidget {
 class _UserListPageState extends State<UserListPage> {
   @override
   Widget build(BuildContext context) {
-    late final StreamUserListController _userListController =
+    late final StreamUserListController userListController =
         StreamUserListController(
       client: StreamChat.of(context).client,
       limit: 25,
@@ -55,31 +61,90 @@ class _UserListPageState extends State<UserListPage> {
     );
 
     return RefreshIndicator(
-      onRefresh: () => _userListController.refresh(),
+      onRefresh: () => userListController.refresh(),
       child: StreamUserListView(
-        onUserTap: (user) async {
-          final client = StreamChat.of(context).client;
+        controller: userListController,
+        itemBuilder: (context, users, index, defaultWidget) {
+          final User user = users[index];
+          final name = user.name;
+          final nativeLanguage = user.extraData['nativeLanguage'].toString();
+          final targetLanguage = user.extraData['targetLanguage'].toString();
+          final isOnline = user.online;
 
-          final channel = client.channel(
-            'messaging',
-            extraData: {
-              'name': '${user.id} | ${client.state.currentUser!.id}',
-              'members': [user.id, client.state.currentUser!.id],
+          return ListTile(
+            onTap: () async {
+              final client = StreamChat.of(context).client;
+
+              final channel = client.channel(
+                'messaging',
+                extraData: {
+                  'name': '${user.id} | ${client.state.currentUser!.id}',
+                  'members': [user.id, client.state.currentUser!.id],
+                },
+              );
+
+              await channel.watch();
+
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => StreamChannel(
+                    channel: channel,
+                    child: const ChannelPage(),
+                  ),
+                ),
+              );
             },
-          );
-
-          await channel.watch();
-
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => StreamChannel(
-                channel: channel,
-                child: const ChannelPage(),
-              ),
+            leading: StreamUserAvatar(
+              user: user,
+              borderRadius: BorderRadius.circular(45),
+              constraints: BoxConstraints.tight(const Size(40.0, 40.0)),
+              showOnlineStatus: false,
+            ),
+            title: Row(
+              children: [
+                Container(
+                  width: 6.0,
+                  height: 6.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: isOnline
+                        ? const Color.fromRGBO(110, 254, 87, 1)
+                        : LightModeColors.grayColor,
+                  ),
+                ),
+                const SizedBox(
+                  width: 8.0,
+                ),
+                Text(
+                  name,
+                  style: const TextStyle(fontSize: 15.0),
+                ),
+                const SizedBox(
+                  width: 8.0,
+                ),
+                Row(children: [
+                  Text(nativeLanguage,
+                      style: const TextStyle(
+                          fontSize: 13.0, color: LightModeColors.grayColor)),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  const Icon(
+                    Icons.arrow_forward,
+                    color: LightModeColors.secondaryColor,
+                    size: 10.0,
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(targetLanguage,
+                      style: const TextStyle(
+                          fontSize: 13.0, color: LightModeColors.blackColor))
+                ])
+              ],
             ),
           );
         },
-        controller: _userListController,
       ),
     );
   }
