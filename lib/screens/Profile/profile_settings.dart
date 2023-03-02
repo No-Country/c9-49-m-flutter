788398d749f273e6b89ae06f1fcc83800324ece4
Tomás Salina <salinatomass53@gmail.com>
2023-03-02 17:46:19 -0300

    Styles in logout modal

--- lib/screens/Profile/profile_settings.dart
+++ lib/screens/Profile/profile_settings.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/Authentication/login_screen.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart' as stream;
-import "./../../../utils/formating_strings.dart";
 import "package:flutter_svg/flutter_svg.dart";
 
 // Services:
@@ -9,6 +8,9 @@ import '../../services/auth_service.dart';
 
 // Types:
 import '../../types/user.dart';
+
+// Modal:
+import '../../widgets/Modals/confirm_logout_modal.dart';
 
 class ProfileSettings extends StatefulWidget {
   const ProfileSettings({super.key, required this.user});
@@ -125,9 +127,6 @@ class _ProfileSettingsState extends State<ProfileSettings> {
                 size: 20,
               ),
             ),
-            // onTap: () {
-
-            // },
 
             const Padding(padding: EdgeInsets.only(bottom: 32)),
 
@@ -148,130 +147,140 @@ class _ProfileSettingsState extends State<ProfileSettings> {
                     ),
                   ),
                   onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return Dialog(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5.0),
-                            ),
-                            backgroundColor: Colors.white,
-                            child: Container(
-                              // width: MediaQuery.of(context).size.width - 5,
+                    showConfirmModal(context,
+                        title: "¡Atención!",
+                        description:
+                            "¿Estás seguro que quieres cerrar sesión en “Speak Easy”?",
+                        onConfirm: () async {
+                      final client = stream.StreamChat.of(context).client;
+                      await _authService.logout(streamClient: client);
+                      Navigator.pushNamed(context, '/login');
+                    });
 
-                              padding: const EdgeInsets.all(0),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Container(
-                                    padding: const EdgeInsets.fromLTRB(
-                                        15, 0, 15, 15),
-                                    child: Column(
-                                      children: [
-                                        Padding(
-                                            padding: EdgeInsets.only(top: 15)),
-                                        const Text(
-                                          '¡Atención!',
-                                          style: TextStyle(
-                                              fontSize: 19.0,
-                                              fontWeight: FontWeight.w700,
-                                              fontStyle: FontStyle.normal),
-                                        ),
-                                        const SizedBox(height: 32.0),
-                                        const Text(
-                                          '¿Estás seguro que quieres cerrar sesión en "Speak Easy"?',
-                                          style: TextStyle(fontSize: 14.0),
-                                          textAlign: TextAlign.center,
-                                        ),
-                                        const SizedBox(height: 20.0),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Container(
-                                              alignment: Alignment.center,
-                                              child: ElevatedButton(
-                                                onPressed: () async {
-                                                  final client =
-                                                      stream.StreamChat.of(
-                                                              context)
-                                                          .client;
-                                                  await _authService.logout(
-                                                      streamClient: client);
-                                                  Navigator.push(context,
-                                                      MaterialPageRoute(
-                                                          builder: (context) {
-                                                    return const LoginScreen();
-                                                  }));
-                                                },
-                                                style: TextButton.styleFrom(
-                                                    shape: RoundedRectangleBorder(
-                                                        borderRadius:
-                                                            BorderRadius
-                                                                .circular(5)),
-                                                    backgroundColor:
-                                                        const Color.fromRGBO(
-                                                            0, 90, 194, 1),
-                                                    padding:
-                                                        const EdgeInsets.all(
-                                                            14.0)),
-                                                child: const Text('Aceptar',
-                                                    style: TextStyle(
-                                                      color: Color.fromRGBO(
-                                                          230, 230, 230, 1),
-                                                      fontSize: 14,
-                                                      fontWeight:
-                                                          FontWeight.w600,
-                                                    )),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Container(
-                                              width: 136,
-                                              alignment: Alignment.center,
-                                              child: ElevatedButton(
-                                                onPressed: () async {
-                                                  {
-                                                    Navigator.of(context).pop();
-                                                  }
-                                                  {}
-                                                },
-                                                style: TextButton.styleFrom(
-                                                    shape: RoundedRectangleBorder(
-                                                        borderRadius:
-                                                            BorderRadius
-                                                                .circular(5)),
-                                                    backgroundColor:
-                                                        const Color.fromRGBO(
-                                                            0, 90, 194, 1),
-                                                    padding:
-                                                        const EdgeInsets.all(
-                                                            14.0)),
-                                                child: const Text('Cancelar',
-                                                    style: TextStyle(
-                                                      color: Color.fromRGBO(
-                                                          230, 230, 230, 1),
-                                                      fontSize: 14,
-                                                      fontWeight:
-                                                          FontWeight.w600,
-                                                    )),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        });
+                    // showDialog(
+                    //     context: context,
+                    //     builder: (BuildContext context) {
+                    //       return Dialog(
+                    //         shape: RoundedRectangleBorder(
+                    //           borderRadius: BorderRadius.circular(5.0),
+                    //         ),
+                    //         backgroundColor: Colors.white,
+                    //         child: Container(
+                    //           // width: MediaQuery.of(context).size.width - 5,
+
+                    //           padding: const EdgeInsets.all(0),
+                    //           child: Column(
+                    //             mainAxisSize: MainAxisSize.min,
+                    //             children: [
+                    //               Container(
+                    //                 padding: const EdgeInsets.fromLTRB(
+                    //                     15, 0, 15, 15),
+                    //                 child: Column(
+                    //                   children: [
+                    //                     const Padding(
+                    //                         padding: EdgeInsets.only(top: 15)),
+                    //                     const Text(
+                    //                       '¡Atención!',
+                    //                       style: TextStyle(
+                    //                           fontSize: 19.0,
+                    //                           fontWeight: FontWeight.w700,
+                    //                           fontStyle: FontStyle.normal),
+                    //                     ),
+                    //                     const SizedBox(height: 32.0),
+                    //                     const Text(
+                    //                       '¿Estás seguro que quieres cerrar sesión en "Speak Easy"?',
+                    //                       style: TextStyle(fontSize: 14.0),
+                    //                       textAlign: TextAlign.center,
+                    //                     ),
+                    //                     const SizedBox(height: 20.0),
+                    //                     Row(
+                    //                       mainAxisAlignment:
+                    //                           MainAxisAlignment.center,
+                    //                       children: [
+                    //                         Container(
+                    //                           alignment: Alignment.center,
+                    //                           child: ElevatedButton(
+                    //                             onPressed: () async {
+                    //                               final client =
+                    //                                   stream.StreamChat.of(
+                    //                                           context)
+                    //                                       .client;
+                    //                               await _authService.logout(
+                    //                                   streamClient: client);
+                    //                               Navigator.push(context,
+                    //                                   MaterialPageRoute(
+                    //                                       builder: (context) {
+                    //                                 return const LoginScreen();
+                    //                               }));
+                    //                             },
+                    //                             style: TextButton.styleFrom(
+                    //                                 shape: RoundedRectangleBorder(
+                    //                                     borderRadius:
+                    //                                         BorderRadius
+                    //                                             .circular(5)),
+                    //                                 backgroundColor:
+                    //                                     const Color.fromRGBO(
+                    //                                         0, 90, 194, 1),
+                    //                                 padding:
+                    //                                     const EdgeInsets.all(
+                    //                                         14.0)),
+                    //                             child: const Text('Aceptar',
+                    //                                 style: TextStyle(
+                    //                                   color: Color.fromRGBO(
+                    //                                       230, 230, 230, 1),
+                    //                                   fontSize: 14,
+                    //                                   fontWeight:
+                    //                                       FontWeight.w600,
+                    //                                 )),
+                    //                           ),
+                    //                         ),
+                    //                       ],
+                    //                     ),
+                    //                     Row(
+                    //                       mainAxisAlignment:
+                    //                           MainAxisAlignment.center,
+                    //                       children: [
+                    //                         Container(
+                    //                           width: 136,
+                    //                           alignment: Alignment.center,
+                    //                           child: ElevatedButton(
+                    //                             onPressed: () async {
+                    //                               {
+                    //                                 Navigator.of(context).pop();
+                    //                               }
+                    //                               {}
+                    //                             },
+                    //                             style: TextButton.styleFrom(
+                    //                                 shape: RoundedRectangleBorder(
+                    //                                     borderRadius:
+                    //                                         BorderRadius
+                    //                                             .circular(5)),
+                    //                                 backgroundColor:
+                    //                                     const Color.fromRGBO(
+                    //                                         0, 90, 194, 1),
+                    //                                 padding:
+                    //                                     const EdgeInsets.all(
+                    //                                         14.0)),
+                    //                             child: const Text('Cancelar',
+                    //                                 style: TextStyle(
+                    //                                   color: Color.fromRGBO(
+                    //                                       230, 230, 230, 1),
+                    //                                   fontSize: 14,
+                    //                                   fontWeight:
+                    //                                       FontWeight.w600,
+                    //                                 )),
+                    //                           ),
+                    //                         ),
+                    //                       ],
+                    //                     ),
+                    //                   ],
+                    //                 ),
+                    //               ),
+                    //             ],
+                    //           ),
+                    //         ),
+                    //       );
+                    //     });
                   },
                 ),
               ],
